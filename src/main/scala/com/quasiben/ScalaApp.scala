0d1cab39ca38487de66e15aef19d4b01dec9f27c
Benjamin Zaitlen <quasiben@gmail.com>
2016-01-18 16:13:51 -0500

    adding command line example

--- src/main/scala/com/quasiben/ScalaApp.scala
+++ src/main/scala/com/quasiben/ScalaApp.scala
@@ -1,11 +1,15 @@
 package com.quasiben
 
+import com.quasiben.ClientArguments.parseArgs
 
 object ScalaApp {
   def main(args: Array[String]) {
     println("Hello World!")
     val a = 10
     println(a)
+    val parsedArgs = parseArgs(args)
+    println(parsedArgs)
+    println("HELLO WORLD")
   }
 }
 
